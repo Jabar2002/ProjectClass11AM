74f87c6845e1f73fa26a0c5f489128064520712e
Jabar2002 <ja9128969@gmail.com>
2025-03-16 16:48:05 +0530

    Commit with local with your repository

--- src/test/java/org/sample/Class11AM.java
+++ src/test/java/org/sample/Class11AM.java
@@ -1,5 +1,13 @@
 package org.sample;
 
 public class Class11AM {
-
+private void methoda1() {
+System.out.println("1");
+}
+private void methoda2() {
+System.out.println("2");
+}
+private void methoda3() {
+System.out.println("3");
+}
 }
